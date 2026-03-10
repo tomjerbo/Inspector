432c7fed1fc194e927ba4e852e4d21281dfa84ea
Jerbo <60573821+tomjerbo@users.noreply.github.com>
2026-03-10 20:06:21 +0100

    fix: snake_case and not setting target each call

--- Editor/ShowIfDrawer.cs
+++ Editor/ShowIfDrawer.cs
@@ -7,9 +7,9 @@ namespace Jerbo.Inspector {
     public class ShowIfDrawer : PropertyDrawer {
 
         const char ARGUMENT_SPLIT_CHARACTER = ' ';
-        ShowIf target;
-        Token[] cachedTokens;
-        Result parseResult;
+        readonly ShowIf target;
+        Token[] cached_tokens;
+        Result parse_result;
         bool is_visible;
         
 
@@ -21,25 +21,23 @@ namespace Jerbo.Inspector {
             InvalidType,
         }
 
+        public ShowIfDrawer() {
+            target = attribute as ShowIf;
+        }
+        
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
             return is_visible ? base.GetPropertyHeight(property, label) : 0f;
         }
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
-            target = attribute as ShowIf;
-            if (target == null) {
-                return;
-            }
-
-            
-            if (parseResult == Result.NotParsed) {
-                parseResult = ParseArgumentString(target.argument, property, out is_visible);
-                if (parseResult != Result.Ok) {
-                    Debug.Log($"Parsing failed! {parseResult}");
+            if (parse_result == Result.NotParsed) {
+                parse_result = parse_argument_string(target.argument, property, out is_visible);
+                if (parse_result != Result.Ok) {
+                    Debug.Log($"Parsing failed! {parse_result}");
                 }
             }
-            else if (parseResult == Result.Ok) {
-                Result result = EvaluateTokens(cachedTokens, property, out is_visible);
+            else if (parse_result == Result.Ok) {
+                Result result = evaluate_tokens(cached_tokens, property, out is_visible);
                 if (result != Result.Ok) {
                     Debug.LogError("Failed to EvaluateTokens: " + result);
                 }
@@ -51,75 +49,75 @@ namespace Jerbo.Inspector {
         }
 
 
-        Result ParseArgumentString(string argument, SerializedProperty property, out bool result) {
+        Result parse_argument_string(string argument, SerializedProperty property, out bool result) {
             result = false;
             if (string.IsNullOrEmpty(argument)) {
                 return Result.InvalidArgumentSyntax;
             }
             
-            cachedTokens = GenerateTokens(argument);
-            return EvaluateTokens(cachedTokens, property, out result);
+            cached_tokens = GenerateTokens(argument);
+            return evaluate_tokens(cached_tokens, property, out result);
         }
         
 
         Token[] GenerateTokens(string argument) {
-            string[] argumentSections = argument.Split(ARGUMENT_SPLIT_CHARACTER, StringSplitOptions.RemoveEmptyEntries);
-            int argCount = argumentSections.Length;
-            Token[] generatedTokens = new Token[argCount];
+            string[] argument_sections = argument.Split(ARGUMENT_SPLIT_CHARACTER, StringSplitOptions.RemoveEmptyEntries);
+            int num_args = argument_sections.Length;
+            Token[] tokens = new Token[num_args];
 
-            for (int i = 0; i < argCount; i++) {
-                if (float.TryParse(argumentSections[i], out float floatValue)) {
-                    generatedTokens[i].tokenValue = floatValue;
-                    generatedTokens[i].type = TokenType.Float;
+            for (int i = 0; i < num_args; i++) {
+                if (float.TryParse(argument_sections[i], out float floatValue)) {
+                    tokens[i].tokenValue = floatValue;
+                    tokens[i].type = TokenType.Float;
                 }
-                else if (int.TryParse(argumentSections[i], out int intValue)) {
-                    generatedTokens[i].tokenValue = intValue;
-                    generatedTokens[i].type = TokenType.Int;
+                else if (int.TryParse(argument_sections[i], out int intValue)) {
+                    tokens[i].tokenValue = intValue;
+                    tokens[i].type = TokenType.Int;
                 }
-                else if (bool.TryParse(argumentSections[i], out bool boolValue)) {
-                    generatedTokens[i].tokenValue = boolValue;
-                    generatedTokens[i].type = TokenType.Bool;
+                else if (bool.TryParse(argument_sections[i], out bool boolValue)) {
+                    tokens[i].tokenValue = boolValue;
+                    tokens[i].type = TokenType.Bool;
                 }
                 else {
-                    switch (argumentSections[i]) {
-                        case "==": generatedTokens[i].type = TokenType.Equals; break;
-                        case "!=": generatedTokens[i].type = TokenType.NotEquals; break;
-                        case ">": generatedTokens[i].type = TokenType.MoreThan; break;
-                        case "<": generatedTokens[i].type = TokenType.LessThan; break;
-                        case ">=": generatedTokens[i].type = TokenType.MoreOrEqual; break;
-                        case "<=": generatedTokens[i].type = TokenType.LessOrEqual; break;
+                    switch (argument_sections[i]) {
+                        case "==": tokens[i].type = TokenType.Equals; break;
+                        case "!=": tokens[i].type = TokenType.NotEquals; break;
+                        case ">": tokens[i].type = TokenType.MoreThan; break;
+                        case "<": tokens[i].type = TokenType.LessThan; break;
+                        case ">=": tokens[i].type = TokenType.MoreOrEqual; break;
+                        case "<=": tokens[i].type = TokenType.LessOrEqual; break;
 
-                        case "&&": generatedTokens[i].type = TokenType.And; break;
-                        case "||": generatedTokens[i].type = TokenType.Or; break;
+                        case "&&": tokens[i].type = TokenType.And; break;
+                        case "||": tokens[i].type = TokenType.Or; break;
                         case "null":
                         {
-                            generatedTokens[i].tokenValue = false;
-                            generatedTokens[i].type = TokenType.Bool;
+                            tokens[i].tokenValue = false;
+                            tokens[i].type = TokenType.Bool;
                             break;
                         }
 
                         default:
-                            generatedTokens[i].type = TokenType.VariableName;
-                            generatedTokens[i].tokenValue = argumentSections[i];
+                            tokens[i].type = TokenType.VariableName;
+                            tokens[i].tokenValue = argument_sections[i];
                             break;
                     }
                 }
             }
 
-            return generatedTokens;
+            return tokens;
         }
         
 
-        Result EvaluateTokens(Token[] tokens, SerializedProperty property, out bool value) {
+        Result evaluate_tokens(Token[] tokens, SerializedProperty property, out bool value) {
             value = false;
 
             if (tokens.Length == 1) {
-                Result result = ResolveValue(tokens[0], property, out object tokenValue);
+                Result result = resolve_token_value(tokens[0], property, out object tokenValue);
                 if (result != Result.Ok) {
                     return result;
                 }
                 
-                value = ConvertToBool(tokenValue);
+                value = convert_token_value_to_bool(tokenValue);
                 return Result.Ok;
             }
 
@@ -129,7 +127,7 @@ namespace Jerbo.Inspector {
             }
 
             int index = 0;
-            Result currentResult = EvaluateComparison(tokens, ref index, property, out bool current);
+            Result currentResult = evaluate_comparison(tokens, ref index, property, out bool current);
             if (currentResult != Result.Ok) return currentResult;
 
             while (index < tokens.Length) {
@@ -140,7 +138,7 @@ namespace Jerbo.Inspector {
 
                 index++;
 
-                Result nextResult = EvaluateComparison(tokens, ref index, property, out bool next);
+                Result nextResult = evaluate_comparison(tokens, ref index, property, out bool next);
                 if (nextResult != Result.Ok) return nextResult;
 
                 if (op == TokenType.And)
@@ -154,7 +152,7 @@ namespace Jerbo.Inspector {
         }
 
         
-        Result EvaluateComparison(Token[] tokens, ref int index, SerializedProperty property, out bool resultValue) {
+        Result evaluate_comparison(Token[] tokens, ref int index, SerializedProperty property, out bool resultValue) {
             resultValue = true;
             
             if (index + 2 >= tokens.Length) {
@@ -162,24 +160,24 @@ namespace Jerbo.Inspector {
             }
 
             
-            Result leftResult = ResolveValue(tokens[index], property, out object left);
+            Result leftResult = resolve_token_value(tokens[index], property, out object left);
             if (leftResult != Result.Ok) {
                 return leftResult;
             }
             
             TokenType op = tokens[index + 1].type;
             
-            Result rightResult = ResolveValue(tokens[index + 2], property, out object right);
+            Result rightResult = resolve_token_value(tokens[index + 2], property, out object right);
             if (rightResult != Result.Ok) {
                 return rightResult;
             }
             index += 3;
 
-            return Compare(left, op, right, out resultValue);
+            return compare(left, op, right, out resultValue);
         }
 
         
-        Result ResolveValue(Token token, SerializedProperty property, out object tokenValue) {
+        Result resolve_token_value(Token token, SerializedProperty property, out object tokenValue) {
             if (token.type != TokenType.VariableName) {
                 tokenValue = token.tokenValue;
                 return Result.Ok;
@@ -216,13 +214,13 @@ namespace Jerbo.Inspector {
             tokenValue = null;
             return Result.InvalidType;
         }
-
         
-        bool ConvertToBool(object v) {
+        bool convert_token_value_to_bool(object v) {
             switch (v) {
-                case bool b: return b;
-                case int i: return i != 0;
+                // Can't really see a good use case for numbers... what should they even evaluate too??
+                case int i: return i > 0;
                 case float f: return Math.Abs(f) > float.Epsilon;
+                case bool b: return b;
                 case UnityEngine.Object o: return o != null;
                 case null: return false;
             }
@@ -231,16 +229,16 @@ namespace Jerbo.Inspector {
         }
         
 
-        Result Compare(object a, TokenType op, object b, out bool evaluation) {
+        Result compare(object a, TokenType op, object b, out bool evaluation) {
             evaluation = true;
             
             if (a is int ai && b is int bi) {
-                evaluation = CompareInt(ai, bi, op);
+                evaluation = compare_int(ai, bi, op);
                 return Result.Ok;
             }
 
             if (a is float af && b is float bf) {
-                evaluation = CompareFloat(af, bf, op);
+                evaluation = compare_float(af, bf, op);
                 return Result.Ok;
             }
 
@@ -263,7 +261,7 @@ namespace Jerbo.Inspector {
             return Result.InvalidComparisson;
         }
 
-        bool CompareFloat(float a, float b, TokenType op) {
+        bool compare_float(float a, float b, TokenType op) {
             switch (op) {
                 case TokenType.Equals: return Mathf.Approximately(a, b);
                 case TokenType.NotEquals: return Mathf.Approximately(a, b) == false;
@@ -276,7 +274,7 @@ namespace Jerbo.Inspector {
             throw new Exception($"Invalid operator {op}");
         }
         
-        bool CompareInt(int a, int b, TokenType op)  {
+        bool compare_int(int a, int b, TokenType op)  {
             switch (op) {
                 case TokenType.Equals: return a == b;
                 case TokenType.NotEquals: return a != b;
